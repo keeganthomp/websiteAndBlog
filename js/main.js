6ed2b7547383706a40af45eb07a438cfe51d4439
keeganthomp <whereiskeegan@gmail.com>
2017-08-01 18:21:42 -0500

    adding mobile funcionality

--- js/main.js
+++ js/main.js
@@ -7,43 +7,56 @@ const orignalPage = main.innerHTML;
 const newDiv = document.createElement("div");
 const newHeader = document.createElement("h1");
 const newParagraph = document.createElement("p");
+const newLink = document.createElement("a");
+const nav = document.createElement("nav");
+const navBar =
+  "<a id='homeLink' class='homeLink' href='./index.html'>Home</a><a id='aboutLink' class='aboutLink' href='#'>About</a><a id='contactLink' class='contactLink' href='#'>Contact</a><a id='skillLink' class='skillLink' href='#'>Skills</a>";
+let homeLink = document.querySelector(".homeLink");
+let contactLink = document.querySelector(".contactLink");
+let aboutLink = document.querySelector(".aboutLink");
+let skillLink = document.querySelector(".skillLink");
+
+aboutLink.addEventListener("click", () => {
+  // newDiv.classList.remove("projectContent");
+  // newParagraph.classList.remove("projects");
+  changeMainContent(content.about.aboutHeader, content.about.aboutContent);
+});
+
+contactLink.addEventListener("click", () => {
+  // newDiv.classList.remove("projectContent");
+  // newParagraph.classList.remove("projects");
+  console.log(contactLink);
+  changeMainContent(
+    content.contact.contactHeader,
+    content.contact.contactContent
+  );
+});
 
 var counter = 0;
 
 const content = {
   about: {
     aboutHeader: "About Me",
-    aboutContent: "My name is Keegan Thompson and I love web development. What interest me most about tech, is knowing that there is always something new to learn. There is never a day that I do not touch Google or Stackoverflow. This fits my personality perfectly. Although I enjoy working with full-stack technologies, I prefer front-end more. My favorite language to work with is Javascript, especially since => ES6. My favorite library is React. On the back-end, I prefer Node.js. PHP, Java, Go, Ruby/Rails, & Elm are technologies I am stoked about working with in the future. Outside of development, I love spending time with wife and puppies, working out, pretending like I have a sense of fashion, and planning my eventual journey around the world."
+    aboutContent:
+      "My name is Keegan Thompson and I love web development. What interest me most about tech, is knowing that there is always something new to learn. There is never a day that I do not touch Google or Stackoverflow. This fits my personality perfectly. Although I enjoy working with full-stack technologies, I prefer front-end more. My favorite language to work with is Javascript, especially since => ES6. My favorite library is React. On the back-end, I prefer Node.js. PHP, Java, Go, Ruby/Rails, & Elm are technologies I am stoked about working with in the future. Outside of development, I love spending time with wife and puppies, working out, pretending like I have a sense of fashion, and planning my eventual journey around the world."
   },
   contact: {
     contactHeader: "Contact Me",
-    contactContent: "Want to work an OS project, talk shop, or send me pictures of your dogs?<br><br> <i class='fa fa-envelope-o' aria-hidden='true'></i> keegant1992@gmail.com <br><i class='fa fa-mobile' aria-hidden='true'></i> (334)-618-9523"
+    contactContent:
+      "Want to work an OS project, talk shop, or send me pictures of your dogs?<br><br> <i class='fa fa-envelope-o' aria-hidden='true'></i> keegant1992@gmail.com <br><i class='fa fa-mobile' aria-hidden='true'></i> (334)-618-9523"
   },
-  projects: {
-    projectHeader: "My Projects",
-    projectContent: [{
-        title: "Title One",
-        img: "http://cdn1-www.dogtime.com/assets/uploads/2011/04/file_2153_column_popular-dog-names.jpg"
-      },
-      {
-        title: "Title Two",
-        img: "http://cdn1-www.dogtime.com/assets/uploads/2011/04/file_2153_column_popular-dog-names.jpg"
-      },
-      {
-        title: "Totle Three",
-        img: "http://cdn1-www.dogtime.com/assets/uploads/2011/04/file_2153_column_popular-dog-names.jpg"
-      },
-      {
-        title: "title Four",
-        img: "http://cdn1-www.dogtime.com/assets/uploads/2011/04/file_2153_column_popular-dog-names.jpg"
-      }
-    ]
+  skills: {
+    skillsHeader: "Skills",
+    skillsContent:
+      "HTML<br><br>CSS<br><br>Javascript<br><br>ES6<br><br>Node.JS<br><br>React<br><br>Redux<br><br>Mongo/Mongoose<br><br>SQL/Sequelize<br><br>Photoshop<br><br>Wordpress<br><br>VS Code<br><br>"
   }
 };
 
 function changeMainContent(h, p) {
   main.innerHTML = "";
   main.appendChild(newDiv);
+  newDiv.appendChild(nav);
+  nav.innerHTML = navBar;
   newDiv.classList.add("myContent");
   newDiv.appendChild(newHeader);
   newHeader.classList.add("contentHeader");
@@ -51,6 +64,27 @@ function changeMainContent(h, p) {
   newDiv.appendChild(newParagraph);
   newParagraph.classList.add("content");
   newParagraph.innerHTML = p;
+  homeLink = document.querySelector("#homeLink");
+  contactLink = document.querySelector("#contactLink");
+  aboutLink = document.querySelector("#aboutLink");
+  skillLink = document.querySelector("#skillLink");
+  aboutLink.addEventListener("click", () => {
+    changeMainContent(content.about.aboutHeader, content.about.aboutContent);
+  });
+  contactLink.addEventListener("click", () => {
+    console.log(contactLink);
+    changeMainContent(
+      content.contact.contactHeader,
+      content.contact.contactContent
+    );
+  });
+  skillLink.addEventListener("click", () => {
+    changeMainContent(
+      content.skills.skillsHeader,
+      content.skills.skillsContent
+    );
+  });
+  console.log(homeLink);
 }
 
 function createImage(i) {
@@ -76,21 +110,15 @@ document.body.addEventListener("keyup", e => {
     );
     counter += 1;
   } else if (e.keyCode == 32 && counter == 2) {
+    newDiv.classList.remove("projectContent");
+    newParagraph.classList.remove("projects");
+
+    changeMainContent(
+      content.skills.skillsHeader,
+      content.skills.skillsContent
+    );
     counter = 0;
     console.log(counter);
-    main.innerHTML = "";
-    newParagraph.innerHTML = "";
-    main.appendChild(newDiv);
-    newDiv.classList.add("projectContent");
-    newDiv.classList.remove("myContent");
-    newDiv.appendChild(newHeader);
-    newHeader.classList.add("contentHeader");
-    newHeader.innerHTML = "My Projects";
-    newDiv.appendChild(newParagraph);
-    newParagraph.classList.add("projects");
-    let projects = content.projects.projectContent.map(e => {
-      createImage(e.img);
-    });
   } else if (e.keyCode == 27) {
     main.innerHTML = orignalPage;
     newDiv.classList.remove("projectContent");
