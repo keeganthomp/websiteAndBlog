065e1c81704e68f30fa6e81e959353cbfe268b39
keeganthomp <whereiskeegan@gmail.com>
2017-08-10 17:58:47 -0500

    adding mobile nav

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,7 +1,35 @@
-document.body.addEventListener("keyup", e =>{
-    if(e.keyCode == 27){
-        window.location.href = "./index.html"
-    } else {
-        alert("press 'esc' to return home");
-    }
-})
+document.body.addEventListener("keyup", e => {
+  if (e.keyCode == 27) {
+    window.location.href = "./index.html";
+  } else {
+    alert("press 'esc' to return home");
+  }
+});
+
+function changeMainContent(h, p) {
+  main.innerHTML = "";
+  main.appendChild(newDiv);
+  newDiv.appendChild(nav);
+  nav.innerHTML = navBar;
+  newDiv.classList.add("myContent");
+  newDiv.appendChild(newHeader);
+  newHeader.classList.add("contentHeader");
+  newHeader.innerHTML = h;
+  newDiv.appendChild(newParagraph);
+  newParagraph.classList.add("content");
+  newParagraph.innerHTML = p;
+  homeLink = document.querySelector("#homeLink");
+  contactLink = document.querySelector("#contactLink");
+  aboutLink = document.querySelector("#aboutLink");
+  portLink = document.querySelector(".portLink");
+  aboutLink.addEventListener("click", () => {
+    changeMainContent(content.about.aboutHeader, content.about.aboutContent);
+  });
+  contactLink.addEventListener("click", () => {
+    console.log(contactLink);
+    changeMainContent(
+      content.contact.contactHeader,
+      content.contact.contactContent
+    );
+  });
+}
